64b7902af5a5375a195dce535e8e51aeecd956d5
smakesoska1 <smakesoska1@etf.unsa.ba>
2019-12-04 16:17:37 +0100

    zavrsen main-podesen resizeable

--- src/ba/unsa/etf/rpr/tutorijal05/Controller.java
+++ src/ba/unsa/etf/rpr/tutorijal05/Controller.java
@@ -1,5 +1,120 @@
 package ba.unsa.etf.rpr.tutorijal05;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+import java.awt.*;
+
+
 public class Controller {
 
+    public boolean decimalni = false;
+    private boolean jednako = false;
+    public Label display;
+    public Button btn0;
+    public Button btn1;
+    public Button btn2;
+    public Button btn3;
+    public Button btn4;
+    public Button btn5;
+    public Button btn6;
+    public Button btn7;
+    public Button btn8;
+    public Button btn9;
+    public Button dotBtn;
+    public Button plusBtn;
+    public Button productBtn;
+    public Button equalsBtn;
+    public Button percentBtn;
+    private String prviBroj = "";
+    private String drugiBroj = "";
+    private String minus = "";
+    private String product = "";
+    private String divide = "";
+    private SimpleStringProperty tekst;
+
+
+    public Controller() {
+        tekst = new SimpleStringProperty("0");
+    }
+
+    public String getText() {
+        return tekst.get();
+    }
+
+    public SimpleStringProperty tekstProperty() {
+        return tekst;
+    }
+
+    public void setTekst(String tekst) {
+        this.tekst.set(tekst);
+    }
+
+    public void zeroClick(ActionEvent actionEvent) { DodajCifru(0);}
+    public void oneClick(ActionEvent actionEvent) { DodajCifru(1);}
+    public void twoClick(javafx.event.ActionEvent actionEvent) { DodajCifru(2); }
+    public void threeClick(javafx.event.ActionEvent actionEvent) { DodajCifru(3); }
+    public void fourClick(javafx.event.ActionEvent actionEvent) { DodajCifru(4); }
+    public void fiveClick(javafx.event.ActionEvent actionEvent) { DodajCifru(5); }
+    public void sixClick(javafx.event.ActionEvent actionEvent) { DodajCifru(6); }
+    public void sevenClick(javafx.event.ActionEvent actionEvent) { DodajCifru(7); }
+    public void eightClick(javafx.event.ActionEvent actionEvent) { DodajCifru(8); }
+    public void nineClick(javafx.event.ActionEvent actionEvent) { DodajCifru(9);}
+    public void dotBtn(javafx.event.ActionEvent actionEvent) {
+        if(decimalni==false)tekst.set(tekst.get()+dotBtn.getText());
+        // if(tekst.get()==".")tekst.set("0.");
+        decimalni=true;
+    }
+    public void plusBtn(javafx.event.ActionEvent actionEvent) {
+        prviBroj=tekst.get();
+        tekst.set("");
+    }
+    public void minusBtn(javafx.event.ActionEvent actionEvent) {
+        minus=tekst.get();
+        tekst.set("");
+    }
+    public void productBtn(javafx.event.ActionEvent actionEvent) {
+        product=tekst.get();
+        tekst.set("");
+    }
+    public void divideBtn(javafx.event.ActionEvent actionEvent) {
+        divide=tekst.get();
+        tekst.set("");
+    }
+
+    void DodajCifru(int broj) {
+        if (tekst.get().equals("0") && tekst.get().length() == 1) {
+            if(broj==0)tekst.set(btn0.getText());
+            else if (broj == 1) tekst.set(btn1.getText());
+            else if (broj == 2) tekst.set(btn2.getText());
+            else if (broj == 3) tekst.set(btn3.getText());
+            else if (broj == 4) tekst.set(btn4.getText());
+            else if (broj == 5) tekst.set(btn5.getText());
+            else if (broj == 6) tekst.set(btn6.getText());
+            else if (broj == 7) tekst.set(btn7.getText());
+            else if (broj == 8) tekst.set(btn8.getText());
+            else tekst.set(btn9.getText());
+        } else {
+            if (broj==0 ) tekst.set(tekst.get() + btn0.getText());
+            else if (broj == 1) tekst.set(tekst.get() + btn1.getText());
+            else if (broj == 2) tekst.set(tekst.get() + btn2.getText());
+            else if (broj == 3) tekst.set(tekst.get() + btn3.getText());
+            else if (broj == 4) tekst.set(tekst.get() + btn4.getText());
+            else if (broj == 5) tekst.set(tekst.get() + btn5.getText());
+            else if (broj == 6) tekst.set(tekst.get() + btn6.getText());
+            else if (broj == 7) tekst.set(tekst.get() + btn7.getText());
+            else if (broj == 8) tekst.set(tekst.get() + btn8.getText());
+            else tekst.set(tekst.get() + btn9.getText());
+        }
+
+    }
+
+
+
+
 }
+
+
+
--- src/ba/unsa/etf/rpr/tutorijal05/Main.java
+++ src/ba/unsa/etf/rpr/tutorijal05/Main.java
@@ -14,6 +14,7 @@ public class Main extends Application {
         primaryStage.setTitle("Digitron");
         primaryStage.setScene(new Scene(root, 200, 275));
         primaryStage.show();
+        primaryStage.setResizable(true);
     }
 
 
